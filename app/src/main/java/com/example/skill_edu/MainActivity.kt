fbe2abc15e816deec11f09944a3f5155e35e9aef
flussigkatz <flussigkatz@gmail.com>
2021-04-02 21:15:56 +0300

    19.2

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -1,38 +1,27 @@
 package com.example.skill_edu
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        val root = findViewById<ViewGroup>(R.id.root)
+//        val view = layoutInflater.inflate(R.layout.activity_main, root, false)
+//        root.addView(view)
+        /*val view1 = layoutInflater.inflate(R.layout.activity_main, root, false)
+        view1.visibility = View.INVISIBLE
+        val view2 = layoutInflater.inflate(R.layout.activity_main, root, false)
+        view2.isEnabled = false
+        val view3 = layoutInflater.inflate(R.layout.activity_main, root, true)
+        view3.setBackgroundColor(resources.getColor(R.color.design_default_color_error))
+        view3.setOnClickListener{}
+        root.addView(view1)
+        root.addView(view2)*/
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
