d8fac164762821a46dc40a9946e0c2bccc6c7857
flussigkatz <flussigkatz@gmail.com>
2021-04-18 11:03:05 +0300

    22.2

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myAnimation = AnimationUtils.loadAnimation(this, R.anim.my_animation)
+
+        image.startAnimation(myAnimation)
     }
 }
