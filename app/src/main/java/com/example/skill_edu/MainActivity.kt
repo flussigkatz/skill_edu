f826ebdd2529cd7162eee733857b193316b7f1d8
flussigkatz <flussigkatz@gmail.com>
2021-04-18 14:04:15 +0300

    22.3

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.skill_edu
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
@@ -13,8 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myAnimation = AnimationUtils.loadAnimation(this, R.anim.my_animation)
 
-        image.startAnimation(myAnimation)
+        val animator = ObjectAnimator.ofFloat(image, View.ALPHA, 0F, 1F)
+        animator.setDuration(1000).start()
+        val animationUpdateListener = object: Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                animator.setFloatValues( 1F, 0F)
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+        }
+
+        animator.addListener(animationUpdateListener)
+
+        start.setOnClickListener { animator.start() }
     }
 }
