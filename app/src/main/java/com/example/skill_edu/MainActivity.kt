a77872988158e265cc50641c1eb42973f00b792f
flussigkatz <flussigkatz@gmail.com>
2021-05-27 21:06:47 +0300

    26.6

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -3,23 +3,10 @@ package com.example.skill_edu
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.content.ContextCompat
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import timber.log.Timber
-import com.example.skill_edu.App
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,14 +14,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         lifecycle.addObserver(LifeCicleListener())
         Timber.d("onCreate")
-
-        val bundle = Bundle()
-        bundle.putParcelable("SomeData",SomeData("SomeText", 777))
         val intent = Intent(this, SecondActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
         findViewById<Button>(R.id.change_text).setOnClickListener {
-//            startActivity(Intent("other"))
-            startActivity(intent)
+            startActivityForResult(intent, 77)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(data == null) {
+            Toast.makeText(this, "null", Toast.LENGTH_SHORT).show()
+            return
+
+        }
+        if (requestCode == 77){
+            if (resultCode == 0) {
+                val string = "${textview.text} ${data?.getStringExtra("result")}"
+                textview.text = string
+                Toast.makeText(this, "Good", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/skill_edu/SecondActivity.kt
+++ app/src/main/java/com/example/skill_edu/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.example.skill_edu
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import com.google.android.material.snackbar.Snackbar
@@ -9,6 +10,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.skill_edu.databinding.ActivitySecondBinding
+import kotlinx.android.synthetic.main.activity_second.*
 import timber.log.Timber
 
 class SecondActivity : AppCompatActivity() {
@@ -17,13 +19,14 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-//        Timber.d(.toString)
-//        val bundle = intent.extras?.getBundle("bundle")
-//        val data = intent.extras?.get("SomeData") as SomeData
-        /*if (data != null) {
-//            findViewById<TextView>(R.id.text_view_2).text =  data.toString()
-//            findViewById<TextView>(R.id.text_view_2).text =  data.value1 + " " +  data.value2
-        }*/
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent()
+        val text = edit_text1.text.toString()
+        intent.putExtra("result", text)
+        setResult(0, intent)
+        super.onBackPressed()
     }
 
 }
