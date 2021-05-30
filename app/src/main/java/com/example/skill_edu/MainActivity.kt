ae460211da8b16e2bb6c1471281a877a370d7aa3
flussigkatz <flussigkatz@gmail.com>
2021-05-30 14:51:15 +0300

    27.1

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -8,14 +8,12 @@ import com.example.skill_edu.cherdak.LifeCicleListener
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         lifecycle.addObserver(LifeCicleListener())
         Timber.d("onCreate")
 
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
 
     }
 
--- app/src/main/java/com/example/skill_edu/cherdak/SomeData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.skill_edu.cherdak
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class SomeData(val value1: String, val value2: Int) : Parcelable {
-}
