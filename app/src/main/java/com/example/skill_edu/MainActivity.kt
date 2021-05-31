1f64977e54c0376e3488681f7315a6857a2c3604
flussigkatz <flussigkatz@gmail.com>
2021-05-31 10:52:47 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.skill_edu
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
+import android.text.Spanned
 import android.text.TextWatcher
 import android.view.WindowManager
 import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import timber.log.Timber
 
@@ -22,15 +25,22 @@ class MainActivity : AppCompatActivity() {
 
         val tv1 = findViewById<TextView>(R.id.tv1)
         val et1 = findViewById<EditText>(R.id.et1)
-        val et2 = findViewById<EditText>(R.id.et2)
 
-        et2.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                tv1.text = et2.text
-                return@setOnEditorActionListener true
+        val inputFilter = InputFilter { source, start, end, dest, dstart, dend ->
+            if (source.length <= 5) {
+                return@InputFilter source
             }
-            return@setOnEditorActionListener false
+            Toast.makeText(this, "Character limit is 5", Toast.LENGTH_SHORT).show()
+            dest.toString()
         }
+        val inputFilter1 = InputFilter { source, start, end, dest, dstart, dend ->
+            if (source.contains("e")) {
+                return@InputFilter dest
+            }
+            Toast.makeText(this, "No e", Toast.LENGTH_SHORT).show()
+            source.toString()
+        }
+        et1.filters = arrayOf(InputFilter.AllCaps(), InputFilter.LengthFilter(2))
 
 
 
