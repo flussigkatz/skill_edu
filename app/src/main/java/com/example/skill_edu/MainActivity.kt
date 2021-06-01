f7cf431ef45b2276214655edb6703b8a3b369ba7
flussigkatz <flussigkatz@gmail.com>
2021-06-01 14:41:58 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.skill_edu
 
+import android.database.Cursor
+import android.database.MatrixCursor
 import android.os.Bundle
+import android.provider.BaseColumns
 import android.view.Menu
 import android.view.WindowManager
 import android.widget.*
@@ -9,6 +12,8 @@ import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.widget.SearchView
 import timber.log.Timber
+import androidx.cursoradapter.widget.SimpleCursorAdapter
+
 
 class MainActivity : AppCompatActivity() {
     private val list = mutableListOf<String>()
@@ -22,6 +27,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         lifecycle.addObserver(LifeCicleListener())
         Timber.d("onCreate")
+        val from = arrayOf("items")
+        val to = intArrayOf(android.R.id.text1)
+        mAdapter = SimpleCursorAdapter(this,
+                R.layout.item,
+                null,
+                from,
+                to,
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER)
 
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN)
 
@@ -29,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         for (i in 1 .. 10) {
             list.add("index$i")
         }
+
 
     }
 
@@ -53,7 +67,7 @@ class MainActivity : AppCompatActivity() {
                 return false
             }
             //Здесь выполняется код при любом изменении текста
-            override fun onQueryTextChange(newText: String?): Boolean {
+            override fun onQueryTextChange(newText: String): Boolean {
                 /*if (list.contains(newText)) {
                     tv1.text = "In list"
                     tv1.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.colorGreen))
@@ -61,10 +75,32 @@ class MainActivity : AppCompatActivity() {
                     tv1.text = "Not in list"
                     tv1.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.colorRed))
                 }*/
+
+                populateAdapter(newText)
                 return false
             }
         })
+        searchView.suggestionsAdapter = mAdapter
+
+        searchView.setOnSuggestionListener(object: SearchView.OnSuggestionListener{
+            override fun onSuggestionSelect(position: Int): Boolean {
+                return true
+            }
+
+            override fun onSuggestionClick(position: Int): Boolean {
+                val cursor: Cursor = mAdapter!!.getItem(position) as Cursor
+                val txt: String = cursor.getString(cursor.getColumnIndex("items"))
+                searchView.setQuery(txt, false)
+                searchView.clearFocus()
+                return true
+            }
+        })
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun populateAdapter(query: String) {
+        val c = MatrixCursor(arrayOf(BaseColumns._ID, "items"))
+//        for (i in suggestions.indices)
     }
 
 
