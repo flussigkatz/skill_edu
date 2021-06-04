d61873a128dc037fcf4ac048977170d1023f9840
flussigkatz <flussigkatz@gmail.com>
2021-06-04 21:30:53 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -13,23 +13,35 @@ import android.widget.FrameLayout
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.scene3.*
-import kotlinx.android.synthetic.main.scene4.*
+import kotlinx.android.synthetic.main.scene3.view.*
+import kotlinx.android.synthetic.main.scene3.view.btn3
+import kotlinx.android.synthetic.main.scene4.view.*
 
 class MainActivity : AppCompatActivity() {
+    private var state = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val scene1 = Scene.getSceneForLayout(scene_root, R.layout.scene1, this)
-        val scene2 = Scene.getSceneForLayout(scene_root, R.layout.scene2, this)
+        val btn4 = findViewById<Button>(R.id.btn3)
+//        val scene1 = Scene.getSceneForLayout(scene_root, R.layout.scene1, this)
+//        val scene2 = Scene.getSceneForLayout(scene_root, R.layout.scene2, this)
         val scene3 = Scene.getSceneForLayout(scene_root, R.layout.scene3, this)
         val scene4 = Scene.getSceneForLayout(scene_root, R.layout.scene4, this)
 //        val transitionManager = TransitionManager()
         val transitionManager = TransitionInflater.from(this).inflateTransitionManager(R.transition.trans_man, scene_root)
-        transitionManager.transitionTo(scene3)
-        btn3.setOnClickListener {
-            transitionManager.transitionTo(scene4)
+        scene3.setEnterAction {
+            scene3.sceneRoot.btn3.setOnClickListener {
+                transitionManager.transitionTo(scene4)
+            }
         }
+
+        scene4.setEnterAction {
+            scene4.sceneRoot.btn3.setOnClickListener {
+                transitionManager.transitionTo(scene3)
+            }
+        }
+        transitionManager.transitionTo(scene3)
 
     }
 
