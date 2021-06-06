a04dc790a3ac1fc863bb4370bf969365b83e2546
flussigkatz <flussigkatz@gmail.com>
2021-06-06 12:29:18 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -2,27 +2,69 @@ package com.example.skill_edu
 
 import android.graphics.Rect
 import android.os.Bundle
-import android.transition.*
-import android.util.TypedValue
-import androidx.appcompat.app.AppCompatActivity
+import android.transition.Explode
+import android.transition.Slide
+import android.transition.Transition
+import android.transition.TransitionManager
+import android.transition.Visibility.MODE_IN
+import android.transition.Visibility.MODE_OUT
+import android.util.Log
+import android.view.Gravity
 import android.view.View
+import android.view.View.*
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.TableRow
+import androidx.annotation.IntDef
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.transition.addListener
+import androidx.core.view.children
+import androidx.core.view.forEach
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.scene3.*
-import kotlinx.android.synthetic.main.scene3.view.*
-import kotlinx.android.synthetic.main.scene3.view.btn3
-import kotlinx.android.synthetic.main.scene4.view.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: MyAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val emptyAdapter = MyAdapter(0)
+        adapter = MyAdapter(9) {
+            val viewRect = Rect()
+            it.getGlobalVisibleRect(viewRect)
+            val slideIn = Slide().apply {
+                mode = MODE_IN
+                duration = 1000
+                slideEdge = Gravity.START
 
+//                epicenterCallback = object : Transition.EpicenterCallback() {
+//                    override fun onGetEpicenter(transition: Transition?): Rect {
+//                        return viewRect
+//                    }
+//                }
+
+            }
+
+            val slideOut = Slide().apply {
+                mode = MODE_OUT
+                duration = 1000
+                slideEdge = Gravity.END
+//                epicenterCallback = object : Transition.EpicenterCallback() {
+//                    override fun onGetEpicenter(transition: Transition?): Rect {
+//                        return viewRect
+//                    }
+//                }
+            }
+
+            slideOut.addListener({ _ ->
+                TransitionManager.beginDelayedTransition(recycler_view, slideIn)
+                recycler_view.adapter = adapter
+                it.setBackgroundResource(R.color.black)
+            })
+            TransitionManager.beginDelayedTransition(recycler_view, slideOut)
+            recycler_view.adapter = emptyAdapter
+            it.setBackgroundResource(R.color.black)
+        }
+
+        recycler_view.adapter = adapter
     }
-
-
 }
---
+++ app/src/main/java/com/example/skill_edu/MyAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.skill_edu
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(val size: Int, val listener: View.OnClickListener = View.OnClickListener {}) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val view = (holder.itemView as TextView)
+        view.text = (position + 1).toString()
+        view.setOnClickListener (listener)
+    }
+
+    override fun getItemCount(): Int {
+        return size
+    }
+
+
+}
