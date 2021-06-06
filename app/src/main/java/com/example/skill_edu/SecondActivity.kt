cfa2b4c664c1588f514002ad73a66891d69c08c5
flussigkatz <flussigkatz@gmail.com>
2021-06-06 17:14:14 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/SecondActivity.kt
+++ app/src/main/java/com/example/skill_edu/SecondActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.Fade
 import android.transition.Slide
+import android.transition.TransitionManager
+import android.transition.TransitionSet
 import android.view.Gravity
+import android.view.View
 import android.view.Window
+import androidx.core.transition.addListener
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,11 +23,28 @@ class SecondActivity : AppCompatActivity() {
             duration = 1000
             excludeTarget(android.R.id.statusBarBackground, true)
             excludeTarget(android.R.id.navigationBarBackground, true)
+            addListener(onStart = {
+                val slideIn = Slide().apply {
+                    mode = Slide.MODE_IN
+                    slideEdge = Gravity.BOTTOM
+                }
+                image.visibility = View.INVISIBLE
+                TransitionManager.beginDelayedTransition(second, slideIn)
+                image.visibility = View.VISIBLE
+            })
         }
-        window.returnTransition = Fade().apply {
-            mode = Fade.MODE_OUT
-            excludeTarget(android.R.id.statusBarBackground, true)
-            excludeTarget(android.R.id.navigationBarBackground, true)
+        window.returnTransition = TransitionSet().apply {
+            addTransition(Slide(Gravity.BOTTOM).apply {
+                mode = Slide.MODE_OUT
+                addTarget(image)
+                removeTarget(second)
+            })
+            addTransition(Slide(Gravity.END).apply {
+                mode = Slide.MODE_OUT
+                excludeTarget(android.R.id.statusBarBackground, true)
+                excludeTarget(android.R.id.navigationBarBackground, true)
+            })
+            ordering = TransitionSet.ORDERING_TOGETHER
         }
     }
 }
