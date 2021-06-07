6cbd52e6eec9e5ac2e7f6a6fd08f74b44644877e
flussigkatz <flussigkatz@gmail.com>
2021-06-07 17:25:39 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -22,6 +22,12 @@ import androidx.core.view.children
 import androidx.core.view.forEach
 import kotlinx.android.synthetic.main.activity_main.*
 import android.util.Pair
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.PagerAdapter
+
+const val NUM_PAGES = 5
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,9 +35,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.contaiber, MainFragment())
-            .addToBackStack("MainFragment")
-            .commit()
+        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
+        view_pager.adapter = pagerAdapter
+
+
+    }
+
+    private inner class ScreenSlidePagerAdapter(fm:FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        override fun getCount(): Int = NUM_PAGES
+
+        override fun getItem(position: Int): Fragment = PageFragment(position)
+
     }
 }
