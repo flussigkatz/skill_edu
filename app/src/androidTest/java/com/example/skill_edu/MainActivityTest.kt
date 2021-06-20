ce85179e1f724da3965db2d4bebfe5c64c7f4126
flussigkatz <flussigkatz@gmail.com>
2021-06-20 17:16:03 +0300

    Commit

--- app/src/androidTest/java/com/example/skill_edu/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/skill_edu/MainActivityTest.kt
@@ -1,87 +1,52 @@
 package com.example.skill_edu
 
-
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.any
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@LargeTest
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
+    @get:Rule
+    val activitiScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
-    @Rule
-    @JvmField
-    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+    private fun inputDataInEditText(id: Int) : ViewAction {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return  allOf(isDisplayed(), isAssignableFrom(View::class.java), withId(R.id.edit_text))
+            }
+
+            override fun getDescription(): String {
+                return "Input text in child view EditText with specified id."
+            }
+
+            override fun perform(uiController: UiController?, view: View?) {
+            }
+            }
+
+        }
 
     @Test
-    fun mainActivityTest() {
-        val materialCheckBox = onView(
-            allOf(withId(R.id.check_box), withText("Item 1"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.recycler_view),
-                        0),
-                    2),
-                isDisplayed()))
-        materialCheckBox.perform(click())
+    fun test() {
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(0, typeText("000")))
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(1, typeText("111")))
+        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(3, typeText("333")))
 
-        val materialCheckBox2 = onView(
-            allOf(withId(R.id.check_box), withText("Item 3"),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.recycler_view),
-                        2),
-                    2),
-                isDisplayed()))
-        materialCheckBox2.perform(click())
 
-        val appCompatEditText = onView(
-            allOf(withId(R.id.edit_text),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.recycler_view),
-                        0),
-                    1),
-                isDisplayed()))
-        appCompatEditText.perform(replaceText("123"), closeSoftKeyboard())
-
-        val appCompatEditText2 = onView(
-            allOf(withId(R.id.edit_text),
-                childAtPosition(
-                    childAtPosition(
-                        withId(R.id.recycler_view),
-                        1),
-                    1),
-                isDisplayed()))
-        appCompatEditText2.perform(typeText("321"), closeSoftKeyboard())
-    }
-
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
     }
 }
