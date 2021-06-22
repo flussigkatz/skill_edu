ca013d755f07f70c2ae121f1e1538e5d8a93f624
flussigkatz <flussigkatz@gmail.com>
2021-06-22 17:49:20 +0300

    Commit

--- app/src/androidTest/java/com/example/skill_edu/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/skill_edu/MainActivityTest.kt
@@ -2,17 +2,16 @@ package com.example.skill_edu
 
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.CoreMatchers.allOf
-import org.hamcrest.CoreMatchers.any
 import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
@@ -28,7 +27,7 @@ class MainActivityTest {
     private fun inputDataInEditText(id: Int) : ViewAction {
         return object : ViewAction {
             override fun getConstraints(): Matcher<View> {
-                return  allOf(isDisplayed(), isAssignableFrom(View::class.java), withId(R.id.edit_text))
+                return  allOf(withId(id))
             }
 
             override fun getDescription(): String {
@@ -36,17 +35,51 @@ class MainActivityTest {
             }
 
             override fun perform(uiController: UiController?, view: View?) {
+//                onView(withId(id)).perform(typeText("000"))
+//                typeText("444")
+
             }
             }
 
         }
 
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+
+    private fun viewById(viewAction: ViewAction) : ViewAction {
+        scrollTo()
+        return typeText("000")
+    }
+
     @Test
     fun test() {
-        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(0, typeText("000")))
-        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(1, typeText("111")))
-        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(3, typeText("333")))
-
-
+//        onView(allOf(withId(R.id.edit_text2), childAtPosition(withId(R.id.recycler_view), 1)))
+//            .perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(1, inputDataInEditText(R.id.edit_text1)))
+        onView( allOf(childAtPosition( childAtPosition( withId(R.id.recycler_view),0),2))).perform( typeText("222"))
+//        onView(allOf(withId(R.id.edit_text2), childAtPosition(withId(R.id.recycler_view), 0))).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(0, inputDataInEditText(R.id.edit_text1)))
+//        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(0, inputDataInEditText(R.id.edit_text)))
+//        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(0, inputDataInEditText(R.id.edit_text)))
+//        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(1, typeText("111")))
+//        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(0, inputDataInEditText(R.id.edit_text1)))
+//        onView(allOf(withId(R.id.edit_text2), childAtPosition(withId(R.id.recycler_view), 0))).perform(typeText("666"))
+//        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition<MyAdapter.MyViewHolder>(1, childAtPosition(1)))
+//        onView(allOf(withId(R.id.edit_text), childAtPosition( childAtPosition( withId(R.id.recycler_view),0), 2), isDisplayed())).perform(typeText("321"), closeSoftKeyboard())
+//        onView(withId(R.id.recycler_view)).perform(RecyclerViewActions.actionOnItem<MyAdapter.MyViewHolder>(withId(R.id.edit_text), typeText("111")))
     }
+
+
 }
