bad5122bf590eea08bd036de8218da6f35a6c4ad
flussigkatz <flussigkatz@gmail.com>
2021-07-05 15:53:40 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.skill_edu
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ObservableField
 import com.example.skill_edu.databinding.ActivityMainBinding
+
+@BindingAdapter(value = ["color1", "color2", "color3"], requireAll = true)
+fun setGradientBackground(view: ImageView, color1: Int, color2: Int, color3: Int) {
+    val colors = intArrayOf(
+        color1, color2, color3
+    )
+    val gd = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, colors)
+    gd.cornerRadius = 0f
+    view.background = gd
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
-            this,
-            R.layout.activity_main
-        )
-        binding.user = ObservableField(User(220.0))
     }
 
 
