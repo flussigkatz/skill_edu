d61e517935527be3b03cf5030615715a1c258bb0
flussigkatz <flussigkatz@gmail.com>
2021-07-17 13:19:08 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -5,12 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gson = Gson()
+
+        val data =  Data("ava",
+            "email",
+            "Ivan",
+            666,
+            "Ivanov")
+        val toJson = gson.toJson(data)
+        println("!!! $toJson")
+
+
     }
+
+    data class Data(
+        val avatar: String,
+        val email: String,
+        val firstName: String,
+        val id: Int,
+        val lastName: String
+    )
 
 
 }
