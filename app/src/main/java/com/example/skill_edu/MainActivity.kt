c38164edc6f851672b22cd1ea34f7d5d8ff1ea8c
flussigkatz <flussigkatz@gmail.com>
2021-07-17 16:07:58 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -20,14 +20,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val gson = Gson()
+
+        Executors.newSingleThreadExecutor().execute {
+
+            val url = URL("https://reqres.in/api/users/2")
+            val connection = url.openConnection() as HttpsURLConnection
+
+
+            try {
+                val br = BufferedReader(InputStreamReader(connection.inputStream))
+                val line = br.readLine()
+                val user = gson.fromJson(line, User::class.java)
+                println("!!! ${line}")
+                println("!!! ${user.data.firstName}")
+                runOnUiThread {
+//                    textview.text = user.data.firstName
+                }
+            } finally {
+                connection.disconnect()
+            }
+
+        }
+
 
         val client = OkHttpClient()
         val request = Request.Builder()
             .url("https://reqres.in/api/users/2")
             .build()
-
-        val gson = Gson()
-        textview.text = "22"
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
@@ -36,11 +56,12 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call, response: Response) {
                 try {
-                    val responseBody = response.body()
-                    println("!!! ${responseBody?.string()}")
-                    gson.fromJson(responseBody, User::class.java)
+                    val responseBodyString = response.body()?.string()
+                    println("!!! ${responseBodyString}")
+                    val user2 = gson.fromJson(responseBodyString, User::class.java)
+                    println("!!! ${user2.data.firstName}")
                     runOnUiThread {
-//                        textview.text = responseBody?.string()
+                        textview.text = user2.data.firstName
                     }
                 } catch (e: Exception) {
                     println(response)
@@ -49,28 +70,5 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-
-        fun setText(t: String) {
-            textview.text = t
-        }
-
-       /*
-       Executors.newSingleThreadExecutor().execute {
-
-            val url = URL("https://reqres.in/api/users/2")
-            val connection = url.openConnection() as HttpsURLConnection
-            val gson = Gson()
-
-            try {
-                val br = BufferedReader(InputStreamReader(connection.inputStream))
-                val line = br.readLine()
-                val user = gson.fromJson(line, User::class.java)
-                println("!!! ${user.data}")
-            } finally {
-                connection.disconnect()
-            }
-        }*/
-
-
     }
 }
