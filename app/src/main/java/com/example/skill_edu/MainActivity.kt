7dc9fff4b224dfca1e0b5e037ff5f09a139cb75f
flussigkatz <flussigkatz@gmail.com>
2021-07-17 14:17:32 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -6,12 +6,33 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.URL
+import java.util.concurrent.Executors
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Executors.newSingleThreadExecutor().execute {
+
+            val url = URL("https://reqres.in/api/users/2")
+            val connection = url.openConnection() as HttpsURLConnection
+            val gson = Gson()
+
+            try {
+                val br = BufferedReader(InputStreamReader(connection.inputStream))
+                val line = br.readLine()
+                val user = gson.fromJson(line, User::class.java)
+                println("!!! ${user.data}")
+            } finally {
+                connection.disconnect()
+            }
+        }
 
 
     }
