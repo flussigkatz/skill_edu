e4c2d5b98110835128186406ee252e307db1414c
flussigkatz <flussigkatz@gmail.com>
2021-07-17 14:40:50 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.net.URL
@@ -18,7 +21,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Executors.newSingleThreadExecutor().execute {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://reqres.in/api/users/2")
+            .build()
+
+        val gson = Gson()
+        textview.text = "22"
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                try {
+                    val responseBody = response.body()
+                    println("!!! ${responseBody?.string()}")
+                    gson.fromJson(responseBody, User::class.java)
+                    runOnUiThread {
+//                        textview.text = responseBody?.string()
+                    }
+                } catch (e: Exception) {
+                    println(response)
+                    e.printStackTrace()
+                }
+            }
+
+        })
+
+        fun setText(t: String) {
+            textview.text = t
+        }
+
+       /*
+       Executors.newSingleThreadExecutor().execute {
 
             val url = URL("https://reqres.in/api/users/2")
             val connection = url.openConnection() as HttpsURLConnection
@@ -32,7 +69,7 @@ class MainActivity : AppCompatActivity() {
             } finally {
                 connection.disconnect()
             }
-        }
+        }*/
 
 
     }
