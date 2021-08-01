4d0480b6684ad1eadc6ed20abd9361a84dc04fe1
flussigkatz <flussigkatz@gmail.com>
2021-08-01 16:09:12 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.skill_edu.databinding.ActivityMainBinding
 import com.google.gson.GsonBuilder
+import com.squareup.picasso.Picasso
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
@@ -21,12 +22,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val link = "https://memepedia.ru/wp-content/uploads/2018/12/in_article_11341c19c0-768x768.jpg"
-        Glide.with(this)
+
+        Picasso.get()
             .load(link)
-            .fitCenter()
+            .fit()
             .placeholder(R.drawable.android_logo)
             .error(R.drawable.ic_launcher_foreground)
             .into(binding.imageView)
-
     }
 }
