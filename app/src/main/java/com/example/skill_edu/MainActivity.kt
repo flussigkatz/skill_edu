8f53a146ac0ec5d9715e274faa72bd58e8e3f091
flussigkatz <flussigkatz@gmail.com>
2021-09-01 11:36:01 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -1,15 +1,62 @@
 package com.example.skill_edu
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import android.view.View
-import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+const val REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_prm)
+
+        btn.setOnClickListener {
+            checkAndLoad()
+        }
+    }
+
+    fun checkAndLoad() {
+        if (checkPermission()) {
+            Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+        } else requestPermission()
+    }
+
+    fun checkPermission(): Boolean {
+        val res = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return res == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            REQUEST_CODE_WRITE_EXTERNAL_STORAGE)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode) {
+            REQUEST_CODE_WRITE_EXTERNAL_STORAGE ->
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    checkAndLoad()
+                }
+        }
     }
 
 
