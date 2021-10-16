92e703a9260b33f9848ecd22a9fdd8480fa1a564
flussigkatz <flussigkatz@gmail.com>
2021-10-16 11:08:40 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -5,11 +5,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scope = CoroutineScope(Job())
+        val jobLazy = retJobLzy(scope)
+
+        scope.launch {
+            launch {
+                repeat(10) {
+                    println("111 $it")
+                    delay(100)
+                }
+            }
+            val jobAsync = async {
+                repeat(10) {
+                    println("222 $it")
+                    delay(100)
+                }
+                "End"
+            }
+
+            println(jobAsync.await())
+
+            repeat(10) {
+                println("333 $it")
+                jobLazy.start()
+                delay(100)
+            }
+
+
+        }
+//        println("!!!!!!!!!!!!!!!!")
+//        Thread.sleep(10000)
+//        println("?????????????????")
+    }
+
+    private fun retJobLzy(scope: CoroutineScope): Job {
+        return scope.launch(start = CoroutineStart.LAZY) {
+            repeat(10) {
+                println("444 $it")
+                delay(100)
+            }
+        }
     }
 
 
