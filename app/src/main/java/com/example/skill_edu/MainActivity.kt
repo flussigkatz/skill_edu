193b6229b9c1e7d2df38c7d4a2282ea3f4da6d1b
flussigkatz <flussigkatz@gmail.com>
2021-10-23 15:51:26 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.disposables.Disposable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.channels.BroadcastChannel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import java.util.function.Consumer
-import kotlin.coroutines.EmptyCoroutineContext
+import io.reactivex.rxjava3.functions.Consumer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,44 +13,21 @@ class MainActivity : AppCompatActivity() {
 
         println("Start")
 
-        val observable = object : Observable<Int>() {
+
+        val flow = object : Observable<Int>() {
             override fun subscribeActual(observer: Observer<in Int>) {
-//                observer.onSubscribe()
-                observer.onNext(1)
-                observer.onNext(2)
+                observer.onNext(5)
                 observer.onNext(3)
                 observer.onNext(4)
-                observer.onNext(5)
                 observer.onComplete()
             }
-
         }
 
-        val observer = object : Observer<Int> {
-            var i = 1
-            override fun onSubscribe(d: Disposable) {
-                println("Sub")
-            }
+        val subscriber = Consumer<Int> { println(it) }
 
-            override fun onNext(t: Int) {
-                println("value $i: $t")
-                i++
-            }
+        val d = flow.subscribe(subscriber)
 
-            override fun onError(e: Throwable) {
-//                e.printStackTrace()
-                println(e.localizedMessage)
-            }
-
-            override fun onComplete() {
-                println("Complete")
-            }
-
-        }
-
-        val d = observable.subscribe(observer)
-
-//        d.dispose()
+        d.dispose()
 
 
     }
