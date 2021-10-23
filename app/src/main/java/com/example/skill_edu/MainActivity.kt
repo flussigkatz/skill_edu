d26428799ac7092c72b087e249883be542614f32
flussigkatz <flussigkatz@gmail.com>
2021-10-23 14:44:22 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.skill_edu
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.util.function.Consumer
 import kotlin.coroutines.EmptyCoroutineContext
 
 class MainActivity : AppCompatActivity() {
@@ -13,27 +17,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val broadcastChannel = BroadcastChannel<Int>(10)
+        println("Start")
 
-        CoroutineScope(EmptyCoroutineContext).launch {
-            repeat(9) {
-                delay(1)
-                broadcastChannel.send(it)
+        val observable = object : Observable<Int>() {
+            override fun subscribeActual(observer: Observer<in Int>) {
+//                observer.onSubscribe()
+                observer.onNext(1)
+                observer.onNext(2)
+                observer.onNext(3)
+                observer.onNext(4)
+                observer.onNext(5)
+                observer.onComplete()
             }
+
         }
 
-        CoroutineScope(EmptyCoroutineContext).launch {
-            val channel = broadcastChannel.openSubscription()
-            for (i in channel) {
-                println("1: $i")
+        val observer = object : Observer<Int> {
+            var i = 1
+            override fun onSubscribe(d: Disposable) {
+                println("Sub")
             }
-        }
-        CoroutineScope(EmptyCoroutineContext).launch {
-            val channel = broadcastChannel.openSubscription()
-            for (i in channel) {
-                println("2: $i")
+
+            override fun onNext(t: Int) {
+                println("value $i: $t")
+                i++
             }
+
+            override fun onError(e: Throwable) {
+//                e.printStackTrace()
+                println(e.localizedMessage)
+            }
+
+            override fun onComplete() {
+                println("Complete")
+            }
+
         }
+
+        val d = observable.subscribe(observer)
+
+//        d.dispose()
+
 
     }
 
