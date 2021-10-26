9ce08ad06b285c4c4f8971c1aaa1357fecd290cd
flussigkatz <flussigkatz@gmail.com>
2021-10-26 15:56:22 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.functions.Consumer
+import io.reactivex.rxjava3.subjects.PublishSubject
+import io.reactivex.rxjava3.subjects.ReplaySubject
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,20 +17,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btn = findViewById<Button>(R.id.btn)
+        var index = 0
 
 
-//        Observable.just(1,2,3,4).subscribe{ println(it.toInt())}
+        val o1 = Observable.interval(1, TimeUnit.SECONDS)
+        val cO = o1.replay()
+        val s1 =cO.subscribe{ println("s1: $it") }
+
+        Thread.sleep(3000)
+
+        val s2 = cO.subscribe{ println("s2: $it") }
+        cO.connect()
 
 
+        Thread.sleep(3000)
 
 
-        /*o.doOnSubscribe { println("Sub") }
-            .doOnNext { println("------------") }
-            .doOnComplete {
-                println("------------")
-                println("End")
-            }
-            .subscribe { println(it) }*/
+        val o = ReplaySubject.create<Int>()
+        o.subscribe{ println(it)}
+
+        btn.setOnClickListener {
+                o.onNext(++index)
+            s1.dispose()
+            s2.dispose()
+            val s3 = cO.subscribe{ println("s3: $it") }
+        }
+
     }
 
 
