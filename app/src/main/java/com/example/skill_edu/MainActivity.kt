d3ff285f0e799fb17728cc799b72e3938c80336a
flussigkatz <flussigkatz@gmail.com>
2021-10-30 16:01:16 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -2,12 +2,17 @@ package com.example.skill_edu
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.functions.Consumer
+import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.PublishSubject
 import io.reactivex.rxjava3.subjects.ReplaySubject
+import java.lang.Thread.currentThread
+import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
@@ -17,31 +22,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btn = findViewById<Button>(R.id.btn)
+        val txt = findViewById<TextView>(R.id.text)
         var index = 0
 
+        println("------------Start------------")
 
-        val o1 = Observable.interval(1, TimeUnit.SECONDS)
-        val cO = o1.replay()
-        val s1 =cO.subscribe{ println("s1: $it") }
+        val o = Observable.just(1, 2)
+            .doOnNext {
+                println("!Emit " + currentThread().name)
+            }
+            .subscribeOn(Schedulers.from(Executors.newSingleThreadExecutor()))
+            .observeOn(Schedulers.single())
+            .map {
+                println("!Map1 " + currentThread().name)
+                it + it
+            }
+            .observeOn(Schedulers.io())
+            .filter {
+                println("!Filter " + currentThread().name)
+                it > 1
+            }
+            .observeOn(Schedulers.trampoline())
+            .map {
+                println("!Map2 " + currentThread().name)
+                it + it
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                txt.text = it.toString()
+                println("!Sub " + currentThread().name)
+            }
 
-        Thread.sleep(3000)
-
-        val s2 = cO.subscribe{ println("s2: $it") }
-        cO.connect()
-
-
-        Thread.sleep(3000)
-
-
-        val o = ReplaySubject.create<Int>()
-        o.subscribe{ println(it)}
-
-        btn.setOnClickListener {
-                o.onNext(++index)
-            s1.dispose()
-            s2.dispose()
-            val s3 = cO.subscribe{ println("s3: $it") }
-        }
 
     }
 
