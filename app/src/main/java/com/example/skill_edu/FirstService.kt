2f74ace55ece6c64325dbff457c8f49aa3295d16
flussigkatz <flussigkatz@gmail.com>
2021-11-13 17:37:16 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/FirstService.kt
+++ app/src/main/java/com/example/skill_edu/FirstService.kt
@@ -2,12 +2,18 @@ package com.example.skill_edu
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import kotlinx.coroutines.*
+import java.util.*
 
 class FirstService : Service() {
 
     private val scope = CoroutineScope(Dispatchers.IO)
+
+    private val binder = LocalBinder()
+
+    private var isActive = false
 
     override fun onCreate() {
         println("!!!onCreate")
@@ -21,13 +27,17 @@ class FirstService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         println("!!!onStartCommand")
-        val extra = intent?.extras?.get("key")
-        scope.launch {
-            repeat(10) {
-                println("!!!$extra: $it")
-                delay(1000)
+        if (!isActive) {
+            val extra = intent?.extras?.get("key")
+            scope.launch {
+                this@FirstService.isActive = true
+                repeat(10) {
+                    println("!!!$extra: $it")
+                    delay(1000)
+                }
+                this@FirstService.isActive = false
+                stopSelf()
             }
-            stopSelf()
         }
         return START_NOT_STICKY
     }
@@ -35,7 +45,13 @@ class FirstService : Service() {
 
 
     override fun onBind(intent: Intent?): IBinder? {
-        return null
+        return binder
+    }
+
+    fun getRandomInt(): Int = Random().nextInt(100)
+
+    inner class LocalBinder: Binder() {
+        fun getService(): FirstService = this@FirstService
     }
 
 }
--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -1,27 +1,59 @@
 package com.example.skill_edu
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import androidx.appcompat.app.AppCompatActivity
 import com.example.skill_edu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var firstService: FirstService
+    private var isBound = false
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as FirstService.LocalBinder
+            firstService = binder.getService()
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isBound = false
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         val intent = Intent(this, FirstService::class.java)
+            intent.also {
+            bindService(it, connection, Context.BIND_AUTO_CREATE)
+        }
         binding.btnStart.setOnClickListener {
-            intent.putExtra("key", "Value")
-            startService(intent)
+            println("!!!btnStart")
+            if (!isBound) return@setOnClickListener
+            binding.txt.text = firstService.getRandomInt().toString()
         }
 
         binding.btnStop.setOnClickListener {
             println("!!!btnStop")
-            stopService(intent)
+//            stopService(intent)
         }
+    }
+
+    override fun onDestroy() {
+        intent.also {
+            unbindService(connection)
+        }
+        super.onDestroy()
     }
 
 
