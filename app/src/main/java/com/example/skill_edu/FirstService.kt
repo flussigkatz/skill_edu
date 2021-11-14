e551a015b03b7556edbe88a5334ac31a7edad9b0
flussigkatz <flussigkatz@gmail.com>
2021-11-14 16:00:31 +0300

    Commit

--- app/src/main/java/com/example/skill_edu/FirstService.kt
+++ app/src/main/java/com/example/skill_edu/FirstService.kt
@@ -1,57 +1,46 @@
 package com.example.skill_edu
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
-import android.os.Binder
+import android.os.Build
 import android.os.IBinder
-import kotlinx.coroutines.*
-import java.util.*
+import androidx.annotation.RequiresApi
 
 class FirstService : Service() {
 
-    private val scope = CoroutineScope(Dispatchers.IO)
 
-    private val binder = LocalBinder()
-
-    private var isActive = false
-
-    override fun onCreate() {
-        println("!!!onCreate")
-    }
-
-    override fun onDestroy() {
-        println("!!!onDestroy")
-        scope.cancel()
-    }
-
-
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        println("!!!onStartCommand")
-        if (!isActive) {
-            val extra = intent?.extras?.get("key")
-            scope.launch {
-                this@FirstService.isActive = true
-                repeat(10) {
-                    println("!!!$extra: $it")
-                    delay(1000)
-                }
-                this@FirstService.isActive = false
-                stopSelf()
-            }
-        }
+        createNotificationChannel()
+
+        val notification: Notification = Notification.Builder(this, "CHANNEL_ID")
+            .setContentTitle("Title1")
+            .setContentText("Some text")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+
+        startForeground(1, notification)
+
         return START_NOT_STICKY
     }
 
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val serviceChannel = NotificationChannel(
+                "CHANNEL_ID",
+                "Foreground Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(serviceChannel)
+        }
+    }
 
 
     override fun onBind(intent: Intent?): IBinder? {
-        return binder
+        return null
     }
-
-    fun getRandomInt(): Int = Random().nextInt(100)
-
-    inner class LocalBinder: Binder() {
-        fun getService(): FirstService = this@FirstService
-    }
-
 }
--- app/src/main/java/com/example/skill_edu/MainActivity.kt
+++ app/src/main/java/com/example/skill_edu/MainActivity.kt
@@ -1,40 +1,35 @@
 package com.example.skill_edu
 
-import android.content.BroadcastReceiver
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
+import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.skill_edu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var service1Intent: Intent
+
     private lateinit var binding: ActivityMainBinding
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val receiver = SecondReceiver()
-        val iFilter = IntentFilter().also {
-            it.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-            it.addAction(Intent.ACTION_BATTERY_CHANGED)
+        service1Intent = Intent(this, FirstService::class.java)
+
+
+        binding.btnStart.setOnClickListener {
+            startForegroundService(service1Intent)
         }
 
-        registerReceiver(receiver, iFilter)
-
-    }
-
-    inner class SecondReceiver : BroadcastReceiver() {
-
-        override fun onReceive(context: Context, intent: Intent) {
-            println("!!! ${intent?.action}")
-//        println("!!! Hi!")
-            Toast.makeText(context, "Hi!", Toast.LENGTH_SHORT).show()
+        binding.btnStop.setOnClickListener {
+            stopService(service1Intent)
         }
     }
+
 
 
 }
