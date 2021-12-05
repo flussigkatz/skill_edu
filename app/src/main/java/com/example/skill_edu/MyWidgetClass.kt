b24d9b752a271809bb23cea8ebbbcf6f46bb8f20
flussigkatz <flussigkatz@gmail.com>
2021-12-05 11:24:09 +0300

    Commit

---
+++ app/src/main/java/com/example/skill_edu/MyWidgetClass.kt
@@ -0,0 +1,60 @@
+package com.example.skill_edu
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.RemoteViews
+
+class MyWidgetClass : AppWidgetProvider() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+    }
+
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        appWidgetIds?.forEach {
+            val pendingIntent = Intent(context, MainActivity::class.java).let { intent ->
+                PendingIntent.getActivity(context, 0, intent, 0)
+            }
+            val views = RemoteViews(
+                context?.packageName,
+                R.layout.app_widget
+            ).apply{
+                setOnClickPendingIntent(R.id.image_widget, pendingIntent)
+            }
+            appWidgetManager?.updateAppWidget(it, views)
+        }
+    }
+
+    override fun onAppWidgetOptionsChanged(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetId: Int,
+        newOptions: Bundle?
+    ) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+    }
+
+    override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
+        super.onDeleted(context, appWidgetIds)
+    }
+
+    override fun onEnabled(context: Context?) {
+        super.onEnabled(context)
+    }
+
+    override fun onDisabled(context: Context?) {
+        super.onDisabled(context)
+    }
+
+    override fun onRestored(context: Context?, oldWidgetIds: IntArray?, newWidgetIds: IntArray?) {
+        super.onRestored(context, oldWidgetIds, newWidgetIds)
+    }
+}
